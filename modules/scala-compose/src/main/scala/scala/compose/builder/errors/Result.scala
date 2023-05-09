fb7ac80b513aea54598375d2276454bb14dd1c26
Jamie Thompson <bishbashboshjt@gmail.com>
2023-05-09 11:24:11 +0200

    reload modules on reload request

--- modules/scala-compose/src/main/scala/scala/compose/builder/errors/Result.scala
+++ modules/scala-compose/src/main/scala/scala/compose/builder/errors/Result.scala
@@ -1,17 +1,23 @@
 package scala.compose.builder.errors
 
 // import scala.util.boundary, boundary.break
+import scala.annotation.targetName
 import scala.util.control.NonLocalReturns.*
 
 enum Result[+T, +E]:
-  case Success[+T](value: T) extends Result[T, Nothing]
-  case Failure[+E](error: E) extends Result[Nothing, E]
+  case Success[+T1](value: T1) extends Result[T1, Nothing]
+  case Failure[+E1](error: E1) extends Result[Nothing, E1]
 
   // inline def ?(using inline canError: CanError[E]): T = this match
+  @targetName("eval")
   inline def ?[E1 >: E](using inline canError: CanError[E1]): T = this match
     case Success(value) => value
     // case err: Failure[E] => break(err)
     case err: Failure[E] => throwReturn(err)
+
+  def toEither: Either[E, T] = this match
+    case Success(value) => Right(value)
+    case Failure(error) => Left(error)
 
 object Result:
   // inline def apply[T, E](inline body: CanError[E] ?=> T): Result[T, E] =
